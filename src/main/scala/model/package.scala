4338223d511dae4ce853f1189c25512b5040aaee
Marek Ślązak <marek.slazak@relayr.io>
2023-07-09 22:27:48 +0200

    Fix datetime parsing

--- src/main/scala/model/package.scala
+++ src/main/scala/model/package.scala
@@ -1,13 +1,15 @@
 import spray.json.{JsString, JsValue, RootJsonFormat}
 
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
+import java.time.format.{DateTimeFormatter, DateTimeParseException}
 
 package object model {
   implicit object ZonedDateTimeFormat extends RootJsonFormat[ZonedDateTime] {
     override def read(json: JsValue): ZonedDateTime = {
+      println(json)
       json match {
-        case str: JsString => ZonedDateTime.parse(str.toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a z"))
+        case str: JsString =>
+          ZonedDateTime.parse(str.toString(), DateTimeFormatter.ofPattern("\"yyyy-MM-dd HH:mm:ss Z\""))
       }
     }
 
